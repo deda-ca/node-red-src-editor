74811a448e739cf2bff0bff07fae7cd12cb16fdf
Charbel Choueiri <charbel.choueiri@gmail.com>
2025-12-22 22:01:30 -0500

    feat: Enhance error handling in postFlows and simplify listenToFlowsChange parameters

--- src/api.ts
+++ src/api.ts
@@ -46,7 +46,8 @@ export async function postFlows(nodeRedUrl: string, bearerToken: string | null, 
 
     // Check the response status
     if (response.status === 200) return response.json();
-    else throw new Error(`Error posting flows to node-red: Status ${response.status}, ${response.statusText}, ${await response.text()}`);
+    else if (response.status === 409) throw new Error('Conflict error, the flows have been modified since last retrieved. Please fetch the latest flows and try again.');
+    else throw new Error(`Posting flows to node-red: ${response.status}, ${response.statusText}, ${await response.text()}`);
 }
 
 /**
@@ -57,61 +58,71 @@ export async function postFlows(nodeRedUrl: string, bearerToken: string | null, 
  * @param {string | null} authToken - The authentication token to use. If no authentication is enabled, set this to null.
  * @param {(event: { topic: string; data: any }) => void} callback - The callback to call when a new flow change is received.
  */
-export function listenToFlowsChange(nodeRedUrl: string, topics: string[], authToken: string | null, callback: (event: { topic: string; data: any }) => void) {
+export function listenToFlowsChange(nodeRedUrl: string, authToken: string | null, callback: (event: { topic: string; data: any }) => void) {
     // Build the URL and create the WebSocket
     nodeRedUrl = nodeRedUrl.endsWith('/') ? `${nodeRedUrl}comms` : `${nodeRedUrl}/comms`;
     const ws = new WebSocket(nodeRedUrl);
+    // The topics to listen to
+    const topics: string[] = ['notification/runtime-deploy'];
 
+    // If disconnected, retry with exponential backoff
     const retryDelay = 1000;
     const maxRetryDelay = 30000;
     let delay = retryDelay;
 
+    // Subscribe to topics
     function subscribe() {
         for (let topic of topics) ws.send(JSON.stringify({ subscribe: topic }));
     }
 
-    // On open
+    // On WebSocket On events. Send auth if needed, then subscribe to topics.
     ws.on('open', () => {
         console.log('WS: connected');
+        // Reset the delay on successful connection
         delay = retryDelay;
-
-        // Auth first
+        // If there is a token then send it.
         if (authToken) ws.send(JSON.stringify({ auth: authToken }));
-        // Then subscribe
+        // Otherwise subscribe to topics immediately.
         else subscribe();
     });
 
+    // On WebSocket message
     ws.on('message', (data) => {
         try {
+            // Parse the event.
             let events = JSON.parse(data.toString());
+            if (!Array.isArray(events)) events = [events];
 
-            // If this is the auth response then subscribe to the next event
-            if (events.auth) {
-                if (events.auth === 'ok') {
-                    subscribe();
-                    console.log('WS: auth ok');
-                } else {
-                    console.error('WS ERROR: auth failed');
+            // Process all the events
+            for (let event of events) {
+                // If this is the auth response then subscribe to the next event
+                if (event.auth) {
+                    if (event.auth === 'ok') {
+                        console.log('WS: auth ok');
+                        subscribe();
+                    } else {
+                        console.error('WS ERROR: auth failed');
+                        // TODO: Close connection and stop retrying?
+                    }
                 }
-            } else {
-                if (!Array.isArray(events)) events = [events];
-                for (let event of events) if (topics.includes(event.topic)) callback(event);
+                // Otherwise if the topic is in the list then call the callback
+                else if (topics.includes(event.topic)) callback(event);
             }
         } catch {
             console.log('WS raw:', data.toString());
         }
     });
 
+    // On WebSocket close, retry connection with exponential backoff
     ws.on('close', () => {
         console.log(`WS closed, retrying in ${delay}ms`);
-        setTimeout(() => listenToFlowsChange(nodeRedUrl, topics, authToken, callback), delay);
+        setTimeout(() => listenToFlowsChange(nodeRedUrl, authToken, callback), delay);
         delay = Math.min(delay * 2, maxRetryDelay);
     });
 
+    // On WebSocket error. Log the error and close the connection to trigger a reconnect.
     ws.on('error', (error) => {
         console.error('WS ERROR:', error);
         ws.close();
     });
-
-    return ws;
 }
