e47a1c81fe9d52b1ded97bfafc9e1c813ce3a158
Charbel Choueiri <charbel.choueiri@gmail.com>
2025-12-22 22:03:44 -0500

    feat: Update config-sample.js to include additional configuration options for source path, clean on start, and file change delay

--- config-sample.js
+++ config-sample.js
@@ -20,6 +20,19 @@ export const config = {
      * The full path where to output the source files.
      * It can be on your local machine and does not need to be located within the node-red folder.
      * The source folder is rebuilt from the latest node-red flows files.
+     *
+     * If not specified then uses the location of the config file as the source path.
      */
-    sourcePath: '~/Projects/iot-node-red-src'
+    sourcePath: '~/Projects/iot-node-red-src',
+
+    /**
+     * If set to true then the source folder will be cleared before building the source files.
+     */
+    cleanOnStart: true,
+
+    /**
+     * Delay in milliseconds to wait after a file change is detected before starting the rebuild process.
+     * This helps to avoid multiple rebuilds when multiple files are changed in quick succession.
+     */
+    fileChangeDelay: 1000
 };
